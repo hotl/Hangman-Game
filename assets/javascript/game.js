16f8a09ea9fc004ef90e2c6a146246a9eec1ee05
Timothy Ho <timothyho0820@gmail.com>
2017-04-09 00:50:04 -0700

    Refactoring code to port functionality into Controller object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,68 +6,74 @@ var tid = setInterval(function() {
 
 
 function gameLogic() {
-	var RG = RandomGenerator();
+	var State = Controller.State();
+	var RG = Controller.Generate();
 	var randomWord = RG.generateRandomWord();
-	document.getElementById('word-blanks').innerHTML = RG.generateUnderscore(randomWord);
+	var wordBlanks = document.getElementById('word-blanks');
+	wordBlanks.innerHTML = RG.generateUnderscore(randomWord);
 	document.onkeypress = function(e) {
 		var userInput = String.fromCharCode(e.keyCode).toLowerCase();
-		if (!/[a-z]/.test(userInput)) return;
+		if (!/^[a-z]+$/.test(userInput)) return;
 
 	};
 };
 
-var State = function() {
-	var numberOfGuesses;
-	var lettersGuessed;
-	var gameOver;
+var Controller = {
+	State: function() {
+		var numberOfGuesses;
+		var lettersGuessed;
+		var gameOver;
 
-	return {
-		initializeState: function() {
-			// implement hard/easy mode, which changes numberOfGuesses
-			// Create HTML input to allow user to choose level of difficulty
-			numberOfGuesses = 12;
-			lettersGuessed = [];
-			gameOver = false;
-		},
-		resetState: function() {
-			this.initializeState();
-		}
-		isGameOver: function() {
-			return gameOver;
-		},
-		decrementGuesses: function() {
-			numberOfGuesses--;
-			return numberOfGuesses;
-		},
-		addLetterGuesses: function(letter) {
-			if (this.isGameOver()) {
-				alert('Game over');
-				return;
+		return {
+			initializeState: function() {
+				// implement hard/easy mode, which changes numberOfGuesses
+				// Create HTML input to allow user to choose level of difficulty
+				numberOfGuesses = 12;
+				lettersGuessed = [];
+				gameOver = false;
+			},
+			resetState: function() {
+				this.initializeState();
+			},
+			isGameOver: function() {
+				return gameOver;
+			},
+			decrementGuesses: function() {
+				numberOfGuesses--;
+				return numberOfGuesses;
+			},
+			addLetterGuesses: function(letter) {
+				if (this.isGameOver()) {
+					alert('Game over');
+					return;
+				}
+				lettersGuessed.push(letter);
+				return lettersGuessed;
 			}
-			lettersGuessed.push(letter);
-			return lettersGuessed;
 		}
+	},
+	Generate: function() {
+		var RandomGenerator = function() {
+			return new RandomGenerator.init();
+		};
+		RandomGenerator.init = function() {
+			this.wordChoices = ['Honda','Toyota','Lexus','BMW','Tesla'];
+			this.generateRandomWord = function() {
+				var wc = this.wordChoices;
+				return wc[Math.floor(Math.random() * wc.length)];
+			};
+			this.generateUnderscore = function(word) {
+				var underscores = [];
+				for (var char in word) {
+					underscores.push('_');
+				}
+				return underscores.join(' ');
+			}
+
+			return this;
+		};
+
+		return RandomGenerator();
 	}
-};
-
-var RandomGenerator = function() {
-	return new RandomGenerator.init();
-}
-
-RandomGenerator.init = function() {
-	this.wordChoices = ['Honda','Toyota','Lexus','BMW','Tesla'];
-	this.generateRandomWord = function() {
-		var wc = this.wordChoices;
-		return wc[Math.floor(Math.random() * wc.length)];
-	};
-	this.generateUnderscore = function(word) {
-		var underscores = [];
-		for (var char in word) {
-			underscores.push('_');
-		}
-		return underscores.join(' ');
-	}
-
-	return this;
 };
 
