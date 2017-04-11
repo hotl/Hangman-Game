6c87970b680b610ef96208af48dde2b917714974
Timothy Ho <timothyho0820@gmail.com>
2017-04-10 23:30:32 -0700

    Fixed DOM Elements function constructor, added renderHTML()

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,28 +4,23 @@ var tid = setInterval(function() {
 	Main();
 }, 100);
 
-var DOMElements = {
-	wordBlanks: document.getElementById('word-blanks'),
-	remainingGuesses: document.getElementById('remaining-guesses'),
-	alreadyGuessed: document.getElementById('already-guessed')
-};
-
 // Imperative Control Flow
+// Main operations executed here
 function Main() {
-	var controller = Controller();
-	var rg = controller.RandomGenerator;
-	var state = controller.State;
+
+	var { controller, RandomGenerator, State } = Controller();
 
 	document.onkeypress = function(e) 
 	{
 		var userInput = String.fromCharCode(e.keyCode).toLowerCase();
 		if (!/^[a-z]+$/.test(userInput)) return;
-		if (state.getGameOver()) {
-			state.resetState();	
+		if (State.getGameOver()) {
+			State.resetState();	
 		}
 		controller.processTurn(userInput);
 	};
 };
+
 
 function Controller() {
 	return new Controller.init();
@@ -34,38 +29,42 @@ function Controller() {
 Controller.init = function() {
 
 	var State = function() {
-		var randomWord, remainingGuesses, lettersGuessed, gameOver;
-		var isGameOver = function() {
-			var guess = Array.from(lettersGuessed).join('');
-			gameOver = remainingGuesses === 0 || guess === randomWord;
-			console.log('guess: ' + guess);
-			console.log('remaining: ' + remainingGuesses);
-			if (guess === randomWord) {
-				// player won
-				// Create new HTML element to update with text saying you won
+		var randomWord, remainingGuesses, alreadyGuessed, gameOver, wordBlanks;
+		var generator = Generator();
+
+		var DOMElements = function() {
+			return {
+				"word-blanks": {
+					id: document.getElementById('word-blanks'),
+					value: wordBlanks
+				},
+				"remaining-guesses": {
+					id: document.getElementById('remaining-guesses'),
+					value: remainingGuesses
+				},
+				"already-guessed": {
+					id: document.getElementById('already-guessed'),
+					value: Array.from(alreadyGuessed)
+				}
 			}
-			else if (remainingGuesses === 0) {
-				// player lost
-			}
-			
-			return gameOver;
 		};
 
 		return {
 			initializeState: function() {
-				randomWord = Generator().generateRandomWord();
+				randomWord = generator.generateRandomWord();
+				wordBlanks = generator.generateUnderscore(randomWord);
+				alreadyGuessed = new Set();
 				remainingGuesses = 12;
-				lettersGuessed = new Set();
 				gameOver = false;
 
 				// Initialize DOMElements
-				DOMElements.wordBlanks.innerHTML = Generator().generateUnderscore(randomWord);
-				DOMElements.remainingGuesses.innerHTML = remainingGuesses;
-				DOMElements.alreadyGuessed.innerHTML = '';
+				this.renderHTML();
 			},
 			resetState: function() {
 				this.initializeState();
 			},
+
+			// Getters & Setters
 			setGameOver: function(gameOver) {
 				this.gameOver = gameOver;
 			},
@@ -76,21 +75,43 @@ Controller.init = function() {
 				remainingGuesses--;
 				return remainingGuesses;
 			},
-			addLetterGuesses: function(letter) {
-				this.decrementGuesses();
-				lettersGuessed.add(letter);
-				if (isGameOver()) {
-					this.resetState();
-					alert('Game over');
-					return;
-				}
-				return lettersGuessed;
-			},
 			getRandomWord: function() {
 				return randomWord;
 			},
 			getRemainingGuesses: function() {
 				return remainingGuesses;
+			},
+
+			// Per-turn methods
+			addLetterGuesses: function(letter) {
+				this.decrementGuesses();
+				alreadyGuessed.add(letter);
+				if (this.isGameOver()) {
+					this.resetState();
+					alert('Game over');
+					return;
+				}
+				this.renderHTML();
+				return alreadyGuessed;
+			},
+			isGameOver: function() {
+				var guess = Array.from(alreadyGuessed).join('');
+				console.log('guess: ' + guess);
+				console.log('remaining: ' + remainingGuesses);
+				if (guess === randomWord.toLowerCase()) {
+					alert('You won');
+				}
+				else if (remainingGuesses === 0) {
+					alert('You lost');
+				}
+				
+				return gameOver;
+			},
+			renderHTML: function() {
+				elements = new DOMElements();
+				for (var key in elements) {
+					elements[key].id.innerHTML = elements[key].value;
+				}
 			}
 		}
 	};
@@ -128,6 +149,7 @@ Controller.init = function() {
 
 	this.State = new State();
 	this.RandomGenerator = Generator();
+	this.controller = this;
 	this.State.initializeState();
 
 	return this;
